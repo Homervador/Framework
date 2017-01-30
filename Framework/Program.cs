81084e46de1f592d34fb1ae4c27d93f230280ccb
homervador <pierre.puss@gmail.com>
2017-01-30 10:25:09 +0100

    premier commit

--- Framework/Program.cs
+++ Framework/Program.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+
 
 namespace Framework
 {
-    class Program
+    public class Blog
     {
-        static void Main(string[] args)
-        {
-        }
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+        public List<Tag> Tags { get; set; }
+    }
+
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public int Intitule { get; set; }
+
+
+        public List<Post>  Posts { get; set; }
+    }
+
+    public class BloggingContext : DtContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
     }
 }
